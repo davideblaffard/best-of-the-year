1a943e84cf25a700ff492c39eb0c4eb74e22ef12
davideblaffard <infodavide98@gmail.com>
2025-02-19 12:54:35 +0100

    add: added Movie and Song classes

--- src/main/java/org/lessons/java/bestof/best_of_the_year/controllers/PageController.java
+++ src/main/java/org/lessons/java/bestof/best_of_the_year/controllers/PageController.java
@@ -1,7 +1,14 @@
 package org.lessons.java.bestof.best_of_the_year.controllers;
 
+import org.lessons.java.bestof.best_of_the_year.models.Movie;
+import org.lessons.java.bestof.best_of_the_year.models.Song;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Arrays;
 
 @Controller
 public class PageController {
@@ -23,7 +30,12 @@ public class PageController {
         return "songs";
     }
 
-    
+    private List<Movie> getBestMovies(){
+        return Arrays.asList(new Movie(1, "Interstellar"), new Movie(2, "Inception"), new Movie(3, "Tenet"));
+    }
 
+    private List<Song> getBestSongs(){
+        return Arrays.asList(new Song(1, "Like him"), new Song(2, "earthquake"), new Song(3, "can i get a kiss"));
+    }
 
 }
